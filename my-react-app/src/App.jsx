5f8fcf7a73f7f0b5733e3c9c07441ce1ce9c6c42
Alesya.Kravchenko <alesya.kravchenko@outlook.com>
2024-03-06 23:10:32 +0100

    renewed basis REACT

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css';
-import {Navigation} from '../components/Navigation'
-import {Header_banner} from '../components/Header_banner'
-import {Categories} from '../components/Categories'
-import {Discount_card}  from '../components/Discount_card'
-import {Sale} from '../components/Sale'
-import {Contact} from '../components/Contact'
-import {Map} from '../components/Map'
+import {Navigation} from './components/Navigation/Navigation'
+import {HeaderBanner} from './components/HeaderBanner/HeaderBanner'
+import {Categories} from './components/Categories/Categories'
+import {DiscountCard}  from './components/DiscountCard/Discount'
+import {Sale} from './components/Sale/Sale'
+import {Contact} from './components/Contact/Contact'
+import {Map} from './components/Map/Map'
 
 export const App = () => {
   return (
     <>
       <Navigation />
-      <Header_banner />
+      <HeaderBanner />
       <Categories />
-      <Discount_card />
+      <DiscountCard />
       <Sale />
       <Contact />
       <Map />
