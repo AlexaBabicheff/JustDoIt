e52e2c1fa23f36c7b23f099181fb101fe2bc7341
Skoryk Anna <annsskorik@gmail.com>
2024-03-12 00:20:35 +0100

    corrected code

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,25 @@
+import React from 'react';
 import './App.css';
-import {Navigation} from '../components/Navigation'
-import {Header_banner} from '../components/Header_banner'
-import {Categories} from '../components/Categories'
-import {Discount_card}  from '../components/Discount_card'
-import {Sale} from '../components/Sale'
-import {Contact} from '../components/Contact'
-import {Map} from '../components/Map'
+import Header from './components/Header/Header'
+import Banner from './components/Banner/Banner'
+import {Categories} from './components/Categories/Categories'
+import {DiscountCard}  from './components/DiscountCard/Discount'
+import {Sale} from './components/Sale/Sale'
+import {Contact} from './components/Contact/Contact'
+import {Map} from './components/Map/Map'
 
 export const App = () => {
   return (
-    <>
-      <Navigation />
-      <Header_banner />
-      <Categories />
-      <Discount_card />
-      <Sale />
-      <Contact />
-      <Map />
+    <> 
+      <Header />
+      <Banner />
+    
+    {/* 
+    //   <Categories />
+    //   <DiscountCard />
+    //   <Sale />
+    //   <Contact />
+    //   <Map /> */}
     </>
   );
 }
