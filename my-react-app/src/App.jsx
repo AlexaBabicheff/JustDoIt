1979f6c1dfa54306780923dfea83691d19b9c5d0
Skoryk Anna <annsskorik@gmail.com>
2024-03-13 21:20:15 +0100

    added toggle and button_discount

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
   return (
     <> 
       <Header />
-      <Banner />
+      {/* <Banner /> */}
       <Outlet/>
     
     {/* 
--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,148 +1,50 @@
 import React from "react";
 import styles from "./Header.module.css";
-import logoImg from "./HeaderImg/logo.svg";
-import iconImg from "./HeaderImg/icons.png";
-import { NavLink } from "react-router-dom";
+import logoImg from "./HeaderImg/logo (1).svg";
+import iconHeart from "./HeaderImg/heart.png";
+import iconBag from "./HeaderImg/icons.png";
+import BtnDarkMode from '../btnDarkMode/BtnDarkMode';
+// import { NavLink } from "react-router-dom";
+// import classes from "./Navigation.module.css";
+// import heart from "../Navigation/NavImg/heart.png";
+// import basket from "../Navigation/NavImg/basket.png";
 
 const Header = () => {
   return (
     <header className={styles}>
-      <nav className={header__nav}>
-        <ul>
-          <div className={logo}>
+
+      <div className="discount">
+            <button1>1 day discount!</button1>
+          </div>
+          {/* <div className="logo">
             <img src={logoImg} alt="Logo" />
           </div>
+          <BtnDarkMode /> */}
+      <nav className="header__nav">
+        <ul>
+          <div className="logo">
+            <img src={logoImg} alt="Logo" />
+          </div>
+           <BtnDarkMode />
           <li>
-      <NavLink
-              to="/"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              Main Page
-            </NavLink>
-            {/* <a href="#!">Main Page</a> */}
-    </li>
-          <li>
-          <NavLink
-              to="/Categories"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              Categories
-            </NavLink>
-            {/* <a href="#!">Categories</a> */}
+            <a href="#!">Main Page</a>
           </li>
           <li>
-          <NavLink
-              to="/All products"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              All products
-            </NavLink>
-            {/* <a href="#!">All products</a> */}
+            <a href="#!">Categories</a>
           </li>
           <li>
-          <NavLink
-              to="/All sales"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              All sales
-            </NavLink>
-            {/* <a href="#!"></a>All sales */}
+            <a href="#!">All products</a>
           </li>
-          <div className={header__icon}>
-            <img src={iconImg} alt="Icon" />
+          <li>
+            <a href="#!"></a>All sales
+          </li>
+          <div className="header_icon">
+            {/* <img src={iconHeart} alt="heart" /> */}
+            <img src={iconBag} alt="bag" />
           </div>
         </ul>
       </nav>
     </header>
   );
 };
-export default Header
-
-/* <header>
-
-<nav>
-  <ul>
-    <li>
-      <NavLink
-              to="/"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              Main Page
-            </NavLink>
-    </li>
-    <li>
-      <NavLink
-              to="/Categories"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              Categories
-            </NavLink>
-    </li>
-    <li>
-      <NavLink
-              to="/All products"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              All products
-            </NavLink>
-      </li>
-    <li>
-      <NavLink
-              to="/All sales"
-              className={(state) => {
-                return state.isPending ? classes.pending : state.isActive ? classes.active : ''
-              }}
-            >
-              All sales
-            </NavLink>
-      </li>
-  </ul>
-</nav>
-<div className="icons">
-  <img src={heart} alt="heart" />
-  <img src={basket} alt="basket" />
-  </div>
-
-  
-</header> 
-    )
-} */
-
-//  <header className={styles}>
-//   {/* <div className="container"> */}
-//     {/* <div className="header__row"> */}
-//     <div className="logo">
-//       {/* <div className="header__logo"> */}
-//         <img src={logoImg} alt="Logo" />
-//         {/* </div> */}
-//       {/* <div className="header__nav"> */}
-//         <nav className="header__nav">
-//         <ul>
-//         {/* <img src={logoImg} alt="Logo" /> */}
-//           <li><a href="#!">Main Page</a></li>
-//           <li><a href="#!">Categories</a></li>
-//           <li><a href="#!">All products</a></li>
-//           <li><a href="#!"></a>All sales</li>
-//         </ul>
-//         </nav>
-//         <div className="header__icon">
-//         <img src={iconImg} alt="Icon" />
-//         </div>
-//       {/* </div> */}
-//     </div>
-//   {/* </div> */}
-// </header>
+export default Header;
