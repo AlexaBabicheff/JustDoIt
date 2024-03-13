6c4c54b67ea1824bec175f1641c1008dc85a0c7c
Alesya.Kravchenko <alesya.kravchenko@outlook.com>
2024-03-14 00:21:42 +0100

    correct version

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,22 @@
 import './App.css';
-// import { Navigation } from './components/Navigation/Navigation';
-// import { HeaderBanner } from './components/HeaderBanner/HeaderBanner';
-// import { Categories } from './components/Categories/Categories';
-// import { DiscountCard } from './components/DiscountCard/Discount';
-import {Sale} from '../src/components/Sale/Sale'; 
-// import { Contact } from './components/Contact/Contact';
-// import { Map } from './components/Map/Map';
+import {Navigation} from './components/Navigation/Navigation'
+import {HeaderBanner} from './components/HeaderBanner/HeaderBanner'
+import {Categories} from './components/Categories/Categories'
+import {DiscountCard}  from './components/DiscountCard/Discount'
+import {Sale} from './components/Sale/Sale'
+import {Contact} from './components/Contact/Contact'
+import {Map} from './components/Map/Map'
 
 export const App = () => {
-    return (
-        <>
-            {/* <Navigation />
-            <HeaderBanner />
-            <Categories />
-            <DiscountCard /> */}
-            <Sale />
-            {/* <Contact />
-            <Map /> */}
-        </>
-    );
-}
-
-
+  return (
+    <>
+      <Navigation />
+      <HeaderBanner />
+      <Categories />
+      <DiscountCard />
+      <Sale />
+      <Contact />
+      <Map />
+    </>
+  );
+  }
