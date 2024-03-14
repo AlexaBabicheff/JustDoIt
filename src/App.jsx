36b18666d39388b801c2b75d05ed810942ff2702
Gismo <117986314+Gismo2000@users.noreply.github.com>
2024-03-14 13:07:50 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+// import {Navigation} from '../components/Navigation'
+// import {Header_banner} from '../components/Header_banner'
+// import {Categories} from '../components/Categories'
+// import {Discount_card}  from '../src/components/Discount_card/Discount.module.jsx'
+// import {Sale} from '../components/Sale'
+import Contacts from '../src/components/Contacts/Contacts.jsx'
+import {Map} from '../src/components/Map/Map.jsx'
+
+export const App = () => {
+  return (
+    <>
+      {/* <Navigation />
+      <Header_banner />
+      <Categories />
+      <Discount_card />
+      <Sale />  */}
+      <Contacts />
+      <Map />
+    </>
+  );
+}
+
