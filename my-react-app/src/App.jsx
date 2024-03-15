ab8c96ab480d93934753b45fbceae89992ed25fc
Alesya.Kravchenko <alesya.kravchenko@outlook.com>
2024-03-15 13:33:38 +0100

    added awasome styles and icons

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
-import Banner from './components/Banner/Banner'
-import {Categories} from './components/Categories/Categories'
-import {DiscountCard}  from './components/DiscountCard/Discount'
+// import Header from './components/Header/Header'
+// import Banner from './components/Banner/Banner'
+import Categories from './components/Categories/Categories'
+// import {DiscountCard}  from './components/DiscountCard/Discount'
 import {Sale} from './components/Sale/Sale'
-import {Contact} from './components/Contact/Contact'
-import {Map} from './components/Map/Map'
+// import {Contact} from './components/Contact/Contact'
+// import {Map} from './components/Map/Map'
 
 export const App = () => {
   return (
     <> 
-      <Header />
-      <Banner /> 
-{/*   <Categories />
-      <DiscountCard /> */}
+      {/* <Header />
+      <Banner />  */}
+      <Categories />
+      {/* <DiscountCard /> */}
       <Sale />
 {/*   <Contact />
       <Map />  */}
