f4a6638913f33d8bde58da09596ca498928b19c1
Alesya.Kravchenko <alesya.kravchenko@outlook.com>
2024-03-18 11:41:12 +0100

    imports added into App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage/HomePage';
+import AllGoods from './components/pages/AllGoods/AllGoods';
+import Bag from './components/pages/Bag/Bag';
+import BurgerMenu from './components/pages/BurgerMenu/BurgerMenu';
+import CategoriesGoods from './components/pages/CategoriesGoods/CategoriesGoods';
+import { CategoriesReview } from './components/pages/CategoriesReview/CategoriesReview';
+import SaleGoods from './components/pages/SaleGoods/SaleGoods';
+import OneProduct from './components/pages/OneProduct/OneProduct';
+import NotFound from './components/pages/NotFound/NotFound';
+import './App.css'; 
+
 function App() {
   return (
     <div className="container">
